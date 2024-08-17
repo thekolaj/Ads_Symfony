5e4fa836d76dad0c40f39b3d60254dff8764ceec
kolaj <thekolaj@gmail.com>
2024-08-17 13:04:29 +0300

    Create base Auth and CRUD with maker

---
+++ src/Controller/AdController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Form\AdType;
+use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/ad', name: 'ad_')]
+class AdController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(AdRepository $adRepository): Response
+    {
+        return $this->render('ad/index.html.twig', [
+            'ads' => $adRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $ad = new Ad();
+        $form = $this->createForm(AdType::class, $ad);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($ad);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('ad_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('ad/new.html.twig', [
+            'ad' => $ad,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(Ad $ad): Response
+    {
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Ad $ad, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AdType::class, $ad);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('ad_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('ad/edit.html.twig', [
+            'ad' => $ad,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, Ad $ad, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$ad->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($ad);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('ad_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
